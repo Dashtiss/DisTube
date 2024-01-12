f80bca2e78ba6e07995f4791d35fe5c1bc3a8bbc
Dashtiss <brandontisserand@outlook.com>
2024-01-12 08:01:36 -0600

    Funny

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+token = os.environ["TOKEN"]
+
+intents = discord.Intents.default()
 
 
-class DisTube
+class DisTube(commands.Bot):
+    async def on_ready(self):
+        super().__init__()
+        print("Ready")
+        await self.tree.sync()
+
+
+@commands.HybridCommand()
+async def searchforvideo(ctx: commands.Context):
